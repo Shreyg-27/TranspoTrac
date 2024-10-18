69b3b9c2f8c3509cdab269101ba2be80494c8eaf
Shreya Ganjoo <98229024+Shreyg-27@users.noreply.github.com>
2024-10-19 01:17:39 +0530

    Add files via upload

---
+++ client/src/components/drivers.js
@@ -0,0 +1,40 @@
+// driversData.js
+const driversData = {
+    success: true,
+    data: [
+      {
+        location: { type: "Point", coordinates: [77.3051, 28.5875] }, // Coordinates for Suresh Gupta in Delhi
+        _id: "6710fbe84bff508a723ee516",
+        email: "sureshgupta@example.com",
+        name: "Suresh Gupta",
+        phone: "456789123",
+        vehicleNumber: "DL 23 345",
+        isAvailable: true,
+        vehicleType: "large",
+      },
+      {
+        location: { type: "Point", coordinates: [72.8777, 19.0760] }, // Coordinates for Mumbai
+        _id: "67100b9eea4824bafa60546d",
+        email: "rakeshkumar@example.com",
+        name: "Rakesh Kumar",
+        phone: "123456789",
+        vehicleNumber: "DL 1A 1234",
+        isAvailable: true,
+        vehicleType: "small",
+      },
+      {
+        location: { type: "Point", coordinates: [88.3639, 22.5726] }, // Coordinates for Kolkata
+        _id: "67100bf8ea4824bafa605470",
+        email: "mohanshyam@example.com",
+        name: "Shyam Mohan",
+        phone: "987654321",
+        vehicleNumber: "DL 1B 5678",
+        isAvailable: true,
+        vehicleType: "medium",
+      },
+      // Add more driver objects as needed
+    ],
+  };
+  
+  export default driversData;
+  
